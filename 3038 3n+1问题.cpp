ca6e9b499e178d540369236e1c8b3b58a4ce9127
wangchz13 <wangchz13@gmail.com>
2016-07-10 16:17:16 +0800

    http://codevs.cn/problem/3038

--- 3038 3n+1问题.cpp
+++ 3038 3n+1问题.cpp
@@ -9,23 +9,12 @@ int f(int n){
 		return 1 + f(n / 2);
 }
 
-int f2(int n) {
-	if (n == 1)
-		return 0;
-	if (n % 2)
-		return 1 + f2((3 * n + 1) / 2);
-	else
-		return 1 + f2(n / 2);
-}
 int main() {
-	/*int T, n;
+	int T, n;
 	cin >> T;
 	for (int i = 0; i < T; i++) {
 		cin >> n;
 		cout << f(n) << endl;
-	}*/
-	int m;
-	cin >> m;
-	cout << f2(m);
+	}
 	return 0;
 }
